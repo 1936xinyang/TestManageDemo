af534f2e949a6e56856be75bee90cd52cda2eb8b
1936xinyang <531582760@qq.com>
2019-04-21 17:56:40 +0800

    配置web api并向webapi添加路由

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //// Web API 配置和服务
+            //// 将 Web API 配置为仅使用不记名令牌身份验证。
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+            //1.默认路由
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional },
+                constraints: new { id = @"\d*" } //约束，这个表示匹配0个或多个数字
+                //constraints: new { id = @"\d+" }  //正则\d+表示匹配一个或多个数字
+            );
+
+            //2.自定义路由一：匹配到action
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "actionapi/{controller}/{action}/{id}",  //将路由模板的前缀改成了“actionapi”。
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //3.自定义路由二
+            config.Routes.MapHttpRoute(
+                name: "TestApi",
+                routeTemplate: "testapi/{controller}/{ordertype}/{id}",
+                defaults: new { ordertype = "aa", id = RouteParameter.Optional }//只要{ordertype}按照路由规则去配置，都能找到对应的方法。
+            );
+        }
+    }
+}
---
+++ WebApi/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.Helper;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/order")] //同一个控制器的所有的action的所有特性路由标识一个相同的前缀，这种做法并非必须，但这样能够增加url的可读性。
+    public class OrderController : ApiController
+    {
+        [HttpGet]
+        public object GetAll()
+        {
+            return "Success";
+        }
+
+        [ActionName("TestApi")]  //如果你想要方法名和action的名称不一致，你也可以自定义action的名称，这个可以通过特性ActionName来实现
+        [Route("{id:int=3}/OrderDetailById")]  //使用“{}”占位符动态传递参数,参数约束:这里约束可变部分{id}的取值必须是int类型。并且默认值是3.
+        [HttpGet]
+        public object GetById(int id)
+        {
+            return "Success" + id;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(string str)
+        {
+            GetDataHelper sqlHelper = new GetDataHelper();
+            switch (str)
+            {
+                case "异步处理"://
+                    return await sqlHelper.GetDataAsync();
+                case "同步处理"://
+                    return sqlHelper.GetData();
+            }
+            return "参数不正确";
+        }       
+    }
+}

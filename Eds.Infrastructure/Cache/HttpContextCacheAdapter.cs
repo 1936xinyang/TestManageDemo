be7bfb5433d6f6d8b2ee30a0cb47e5d1ae422d79
chenhua <531582760@qq.com>
2019-11-24 12:14:00 +0800

    使用ant orm

---
+++ Eds.Infrastructure/Cache/HttpContextCacheAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Eds.Infrastructure.Cache
+{
+    /// <summary>
+    /// HttpContext缓存的包装器，HttpContextCacheAdapter包装了HttpContext.Current.Cache实例，并在实现ICacheStorage接口契约时将工作委托给它。
+    /// </summary>
+    public class HttpContextCacheAdapter : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+
+        public void Store(string key, object data)
+        {
+            HttpContext.Current.Cache.Insert(key, data);
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            T itemStored = (T)HttpContext.Current.Cache.Get(key);
+            if (itemStored == null)
+                itemStored = default(T);
+
+            return itemStored;
+        }
+    }
+}
